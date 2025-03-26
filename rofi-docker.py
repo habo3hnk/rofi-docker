084b8e8099402ab5d3fc7ba32982b6e36d4efcab
habo3hnk <habo3hnk>
2025-03-26 00:55:08 +0000

    feature/replace-subprocess-with-sh

--- rofi-docker.py
+++ rofi-docker.py
@@ -1,103 +1,101 @@
 #!/usr/bin/env python
 
-import subprocess
-import shlex
+import sh
 import sys
-
-
-def run_command(command: str) -> subprocess.CompletedProcess:
-    result = subprocess.run(shlex.split(command), capture_output=True, text=True)
-    return result
 
 
 def rofi_menu(options: list, selected_option: str = "0") -> tuple:
     options_str = "\n".join(options)
 
-    rofi_command = ["rofi", "-dmenu", "-i"]
+    rofi_cmd = sh.Command("rofi")
+    rofi_command = ["-dmenu", "-i"]
     if selected_option:
         rofi_command.extend(["-selected-row", selected_option])
 
-    result = subprocess.run(
-        rofi_command, input=options_str, capture_output=True, text=True
-    ).stdout
-
+    result = rofi_cmd(rofi_command, _in=options_str)
     current_choise = "0"
-
     if result:
+        result = result.strip("\n")
         current_choise = options.index(result.strip("\n"))
-    return (result.strip(), str(current_choise))
+
+    return (result, current_choise)
 
 
 def get_container_list() -> list[str]:
-    containers_result = run_command(
-        "docker ps -a --format '{{.ID}}\t{{.Names}}\t{{.Status}}\t{{.Ports}}'"
-    )
+    try:
+        containers = sh.docker(
+            "ps", "-a", "--format", "{{.ID}}\t{{.Names}}\t{{.Status}}\t{{.Ports}}"
+        )
+        containers = containers.splitlines()
+    except sh.ErrorReturnCode:
+        containers = []
 
-    containers = containers_result.stdout.splitlines()
     containers.append("Stop Docker")
-
-    if not containers:
-        containers = ["Stop Docker"]
-
     return containers
 
 
 def main(previous_select: str = "0") -> None:
-    docker_status = run_command("systemctl is-active docker")
-    if docker_status.returncode != 0:
+    try:
+        docker_status = sh.systemctl("is-active", "docker").strip()
+        if docker_status != "active":
+            action, _ = rofi_menu(["Start Docker"])
+
+            if action == "Start Docker":
+                sh.systemctl("start", "docker")
+                main()
+            elif action == "Exit":
+                sys.exit(1)
+            else:
+                sys.exit(1)
+            return
+    except sh.ErrorReturnCode:
         action, _ = rofi_menu(["Start Docker"])
 
         if action == "Start Docker":
-            start_result = run_command("systemctl start docker")
-            if start_result.returncode == 0:
-                main()
-            else:
-                sys.exit(1)
+            sh.systemctl("start", "docker")
+            main()
         elif action == "Exit":
-            print("Exiting...")
             sys.exit(1)
         else:
-            print("Invalid action")
             sys.exit(1)
+        return
 
     containers = get_container_list()
 
     selected = ""
-    current_choise = ""
+    current_choice = ""
     if previous_select != "":
-        selected, current_choise = rofi_menu(
-            options=containers, selected_option=previous_select
+        selected, current_choice = rofi_menu(
+            options=containers, selected_option=str(previous_select)
         )
 
     if not selected:
-        print("No container selected")
         sys.exit(1)
 
     container_id = selected.split()[0]
 
     if selected == "Stop Docker":
-        run_command("systemctl stop docker")
+        sh.systemctl("stop", "docker")
         sys.exit(0)
 
     action, _ = rofi_menu(["Start", "Stop", "Restart", "Remove", "Back"])
 
     if action == "Start":
-        run_command(f"docker start {container_id}")
-        main(previous_select=current_choise)
+        sh.docker("start", container_id)
+        main(previous_select=current_choice)
     elif action == "Stop":
-        run_command(f"docker stop {container_id}")
-        main(previous_select=current_choise)
+        sh.docker("stop", container_id)
+        main(previous_select=current_choice)
     elif action == "Restart":
-        run_command(f"docker restart {container_id}")
-        main(previous_select=current_choise)
+        sh.docker("restart", container_id)
+        main(previous_select=current_choice)
     elif action == "Remove":
-        run_command(f"docker stop {container_id}")
-        run_command(f"docker rm {container_id}")
+        sh.docker("stop", container_id)
+        sh.docker("rm", container_id)
         main()
     elif action == "Back":
-        main(previous_select=current_choise)
+        main(previous_select=current_choice)
     else:
-        print("Invalid action")
         sys.exit(0)
 
 
